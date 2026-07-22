c3bf751f2a3dcf32907453734c22863391cad913
RealRyanJames <realcoder2023@gmail.com>
2026-07-22 00:58:47 -0400

    Added Structs and imps

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,30 @@ fn create_date() {
     println!("{}", now.format("%Y-%m-%d %H:%M:%S"));
 }
 
+pub struct WebRoute {
+    pub google: String,
+    pub yt: String,
+    pub gmail: String,
+}
+
+pub trait Routes {
+    fn get_web_site(&self) -> String;
+}
+
+impl Routes for WebRoute {
+    fn get_web_site(&self) -> String {
+        format!("{}", self.google)
+    }
+}
+
+pub fn get_web_route(web_route: WebRoute, str: String) -> String {
+    if str.contains(&web_route.google) {
+        return format!("{}", "http://www.google.com");
+    }
+
+    return format!("{}", web_route.google);
+}
+
 #[tokio::main]
 async fn main() {
     let mut site = String::new();
@@ -22,25 +46,65 @@ async fn main() {
             println!("{}", "Enter Site Name: ".to_uppercase());
 
             stdin().read_line(&mut site).unwrap();
-            Command::new("cmd")
-                .arg("/C")
-                .arg("C:\\Program Files\\Mozilla Firefox\\Firefox.exe")
-                .arg(site.to_string())
-                .spawn()
-                .expect("No Site");
 
-            let file: File = File::create("commands.txt").await.expect("NO File Created");
-            write_to_file(file, String::from(site.as_str())).await;
-        } else {
-            println!("{}", "Enter Site Name: ".to_uppercase());
+            let website: WebRoute = WebRoute {
+                gmail: String::from(format!("{}", "http://www.google.com/gmail")),
+                google: String::from(format!("{}", "")),
+                yt: String::from(format!("{}", "")),
+            };
 
-            stdin().read_line(&mut site).unwrap();
-            Command::new("cmd")
-                .arg("/C")
-                .arg(browser)
-                .arg(site.to_string())
-                .spawn()
-                .expect("No Site");
+            let website_yt: WebRoute = WebRoute {
+                gmail: String::from(format!("{}", "http://www.google.com/youtube")),
+                google: String::from(format!("{}", "")),
+                yt: String::from(format!("{}", "")),
+            };
+
+            let website_gmail: WebRoute = WebRoute {
+                gmail: String::from(format!("{}", "http://www.google.com/gmail")),
+                google: String::from(format!("{}", "")),
+                yt: String::from(format!("{}", "")),
+            };
+
+            if site.contains("Google") {
+                Command::new("cmd")
+                    .arg("/C")
+                    .arg("C:\\Program Files\\Mozilla Firefox\\Firefox.exe")
+                    .arg(String::from(get_web_route(
+                        website,
+                        String::from(site.as_str()),
+                    )))
+                    .spawn()
+                    .expect("No Site");
+
+                let file: File = File::create("commands.txt").await.expect("NO File Created");
+                write_to_file(file, String::from(site.as_str())).await;
+            } else if site.contains("YouTube") {
+                Command::new("cmd")
+                    .arg("/C")
+                    .arg("C:\\Program Files\\Mozilla Firefox\\Firefox.exe")
+                    .arg(String::from(get_web_route(
+                        website_yt,
+                        String::from(site.as_str()),
+                    )))
+                    .spawn()
+                    .expect("No Site");
+
+                let file: File = File::create("commands.txt").await.expect("NO File Created");
+                write_to_file(file, String::from(site.as_str())).await;
+            } else if site.contains("Gmail") {
+                Command::new("cmd")
+                    .arg("/C")
+                    .arg("C:\\Program Files\\Mozilla Firefox\\Firefox.exe")
+                    .arg(String::from(get_web_route(
+                        website_gmail,
+                        String::from(site.as_str()),
+                    )))
+                    .spawn()
+                    .expect("No Site");
+
+                let file: File = File::create("commands.txt").await.expect("NO File Created");
+                write_to_file(file, String::from(site.as_str())).await;
+            }
         }
 
         let file: File = File::create("commands.txt").await.expect("NO File Created");
